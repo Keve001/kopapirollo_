2ec173adc7d8116af9a4165eca3d0deee554674e
Keve Lengyel <keve300@gmail.com>
2020-10-22 17:37:56 +0200

    5

--- app/src/main/java/com/example/kopapirollo/MainActivity.java
+++ app/src/main/java/com/example/kopapirollo/MainActivity.java
@@ -20,13 +20,116 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonKo, buttonOllo, buttonPapir;
     private Random random;
     private TextView dontetlen;
-    private int db,dontetlenPont,maxJatekosHp,maxGepHp;
+    private int valasztas,dontetlenPont,maxJatekosHp,maxGepHp,rand;
+    private Button koButton, olloButton, papirButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+
+        koButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valasztas = 1;
+                sajatValasztas.setImageResource(R.drawable.rock);
+
+
+
+            }
+        });
+        papirButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valasztas = 2;
+                sajatValasztas.setImageResource(R.drawable.paper);
+
+            }
+        });
+
+        olloButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valasztas = 3;
+                sajatValasztas.setImageResource(R.drawable.scissors);
+
+            }
+        });
+    }
+    private void gepValasztasa() {
+        rand = random.nextInt(3) + 1;
+        if (rand == 1) {
+            gepValasztas.setImageResource(R.drawable.rock);
+        } else if (rand == 2) {
+            gepValasztas.setImageResource(R.drawable.paper);
+        } else if (rand == 3) {
+            gepValasztas.setImageResource(R.drawable.scissors);
+        }
+    }
+    private void jatekosEletvesztes() {
+        switch (maxJatekosHp) {
+            case 3:
+                jatekos_hp3.setImageResource(R.drawable.heart1);
+                maxJatekosHp--;
+                break;
+            case 2:
+                jatekos_hp2.setImageResource(R.drawable.heart1);
+                maxJatekosHp--;
+                break;
+            case 1:
+                jatekos_hp1.setImageResource(R.drawable.heart1);
+                maxJatekosHp--;
+                break;
+            default:
+                break;
+        }
+    }
+    private void gepEletvesztes ()
+        {
+            switch (maxGepHp) {
+                case 3:
+                    gep_hp3.setImageResource(R.drawable.heart1);
+                    maxGepHp--;
+                    break;
+                case 2:
+                    gep_hp2.setImageResource(R.drawable.heart1);
+                    maxGepHp--;
+                    break;
+                case 1:
+                    gep_hp1.setImageResource(R.drawable.heart1);
+                    maxGepHp--;
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void jatek () {
+            if (valasztas == 1 && rand == 3) {
+                gepEletvesztes();
+            } else if (valasztas == 1 && rand == 2) {
+                jatekosEletvesztes();
+            } else if (valasztas == 2 && rand == 1) {
+                gepEletvesztes();
+            } else if (valasztas == 2 && rand == 3) {
+                jatekosEletvesztes();
+            } else if (valasztas == 3 && rand == 1) {
+                jatekosEletvesztes();
+            } else if (valasztas == 3 && rand == 2) {
+                gepEletvesztes();
+            } else if (valasztas == 1 && rand == 1) {
+                dontetlenPont++;
+                dontetlen.setText("döntetlenek" + dontetlenPont);
+            } else if (valasztas == 2 && rand == 2) {
+                dontetlenPont++;
+                dontetlen.setText("döntetlenek" + dontetlenPont);
+            } else if (valasztas == 3 && rand == 3) {
+                dontetlenPont++;
+                dontetlen.setText("döntetlenek:" + dontetlenPont);
+            }
+        }
+
         private void init()
         {
             sajatValasztas = findViewById(R.id.sajat);
@@ -35,22 +138,21 @@ public class MainActivity extends AppCompatActivity {
             buttonOllo = findViewById(R.id.ollo);
             buttonPapir = findViewById(R.id.papir);
             dontetlen = findViewById(R.id.dontetlen);
-            db = 0;
             random = new Random();
+            valasztas = 0;
             dontetlenPont = 0;
+
             maxJatekosHp = 3;
-            maxGepHp = 3;
             jatekos_hp1 = findViewById(R.id.jatekos_hp1);
             jatekos_hp2 = findViewById(R.id.jatekos_hp2);
             jatekos_hp3 = findViewById(R.id.jatekos_hp3);
 
+            maxGepHp = 3;
             gep_hp1 = findViewById(R.id.gep_hp1);
             gep_hp2 = findViewById(R.id.gep_hp2);
             gep_hp3 = findViewById(R.id.gep_hp3);
         }
-
     }
-
 }
 
 
